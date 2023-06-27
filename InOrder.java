f1c0c5943549ca5529f3628b11822fa909a77a5b
Yash Sharma <yash304sharma@gmail.com>
2023-06-28 00:16:21 +0530

    Add files via upload

---
+++ InOrder.java
@@ -0,0 +1,44 @@
+public class InOrder{
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+        Node(int data){
+            this.data=data;
+            this.right=null;
+            this.left=null;
+        }
+
+    }
+    static class BinaryTree{
+        static int idx=-1;
+        public static Node BuildTree(int nodes[]){
+            idx++;
+            if(nodes[idx]==-1){
+                return null;
+            }
+            Node newNode=new Node(nodes[idx]);
+            newNode.left=BuildTree(nodes);
+            newNode.right=BuildTree(nodes);
+            return newNode;
+        }
+    }
+
+    public static void inorder(Node root){
+        //left-root-right
+        if(root==null){
+            return;
+        }
+        inorder(root.left);
+        System.out.print(root.data+" ");
+        inorder(root.right);
+    }
+    public static void main(String[] args){
+        int nodes[]={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+        BinaryTree tree=new BinaryTree();
+        Node root=tree.BuildTree(nodes);
+        System.out.println(root.data);
+        inorder(root);
+
+    }
+}
---
+++ PreOrder.java
@@ -0,0 +1,47 @@
+public class PreOrder{
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+        Node(int data){
+            this.data=data;
+            this.right=null;
+            this.left=null;
+        }
+
+    }
+
+    public static void preorder(Node root){
+        //root-left-right
+        if(root==null){
+            return;
+        }
+        System.out.print(root.data+" ");
+        preorder(root.left);
+        preorder(root.right);
+    }
+    
+
+    static class BinaryTree{
+        static int idx=-1;
+        public static Node BuildTree(int nodes[]){
+            idx++;
+            if(nodes[idx]==-1){
+                return null;
+            }
+            Node newNode=new Node(nodes[idx]);
+            newNode.left=BuildTree(nodes);
+            newNode.right=BuildTree(nodes);
+            return newNode;
+        }
+    }
+    public static void main(String[] args){
+        int nodes[]={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+        BinaryTree tree=new BinaryTree();
+        Node root=tree.BuildTree(nodes);
+        System.out.println(root.data);
+        preorder(root);
+
+
+    }
+}
