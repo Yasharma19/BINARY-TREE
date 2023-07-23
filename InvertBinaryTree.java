f58d922a8b29e1758cf66bba42c8a6861e36ff0e
Yash Sharma <yash304sharma@gmail.com>
2023-07-24 00:40:04 +0530

    Create InvertBinaryTree.java

---
+++ InvertBinaryTree.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+ 
+class Solution {
+
+    public static void swap(TreeNode root){
+        if(root==null){
+            return;
+        }
+        swap(root.left);
+        swap(root.right);
+
+        TreeNode temp;
+        temp=root.left;
+        root.left=root.right;
+        root.right=temp;
+    }
+
+    public TreeNode invertTree(TreeNode root) {
+    /*MIRROR IMAGE OF TREE*/
+    swap(root);
+    return root;
+
+    }
+}
