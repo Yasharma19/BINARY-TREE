e988aed61072f13a3294de8980965c2efc667e54
Yash Sharma <yash304sharma@gmail.com>
2023-07-28 23:31:19 +0530

    Create CheckForBalancedTree.java

---
+++ CheckForBalancedTree.java
@@ -0,0 +1,31 @@
+class Tree
+{
+     //We have to check with each that the difference between left and right Node will be not more than one. 
+    int height(Node root){
+        if(root==null){
+            return 0;
+        }
+        
+        int l=height(root.left);
+        if(l==-1)
+            return -1;
+        
+        int r=height(root.right);
+        if(r==-1)
+            return -1;
+            
+        if(Math.abs(l-r)>1){
+            return -1;
+        }
+        return Math.max(l,r)+1;
+    }
+    
+    //Function to check whether a binary tree is balanced or not.
+    boolean isBalanced(Node root)
+    {
+        if(height(root)==-1)
+            return false;
+        
+        return true;
+    }
+}
