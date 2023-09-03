6a93b519ece4747930563f06064647ee02ddea23
Yash Sharma <yash304sharma@gmail.com>
2023-09-04 01:06:16 +0530

    Update LeftView.java

--- LeftView.java
+++ LeftView.java
@@ -1,6 +1,5 @@
 class Tree
-{
-    
+{   
     void Left(Node root,ArrayList<Integer> arr,int order){
         if(root==null){
             return;
@@ -8,15 +7,16 @@ class Tree
         if(order==arr.size()){
             arr.add(root.data);
         }
-        Left(root.left,arr,order+1);     //Priority to left as we are doing leftview.
-        Left(root.right,arr,order+1);  //if not present in left, then we find in riight part whether there is a node or not on next level.
+        Left(root.left,arr,order+1);  //Left on priority 
+        Left(root.right,arr,order+1);   //If left not present then add right
     }
     
     //Function to return list containing elements of left view of binary tree.
     ArrayList<Integer> leftView(Node root)
     {
-    ArrayList<Integer> arr=new ArrayList<Integer>();
-    Left(root,arr,0);
-    return arr;
+        ArrayList<Integer> arr=new ArrayList<>();
+        int order=0;
+        Left(root,arr,order);
+        return arr;
     }
 }
