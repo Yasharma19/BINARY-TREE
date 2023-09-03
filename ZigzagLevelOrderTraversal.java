ae51ac878ec6f9e73dbb1b51cb9c93d05d6c5f8f
Yash Sharma <yash304sharma@gmail.com>
2023-09-04 01:47:48 +0530

    Update ZigzagLevelOrderTraversal.java

--- ZigzagLevelOrderTraversal.java
+++ ZigzagLevelOrderTraversal.java
@@ -1,35 +1,39 @@
-class HelloWorld{
-    public List<List<Integer>> ZigZag(TreeNode root){
-        List<List<Integer>> ans=new ArraysList<>();
-        if(root==null){
-            return ans;
-        }
-        Queue<TreeNode> q=new LinkedList<>();    
+import java.util.*;
+
+// We have to reverse the nodes of the alternate levels of the tree
+// using STACK for reversing the nodes as it uses LIFO
+
+class HelloWorld {
+    public static List<List<Integer>> ZigZag(TreeNode root){
+        List<List<Integer>> result=new ArrayList<>();
+        Queue<TreeNode> q=new LinkedList<>();
         q.add(root);
         boolean flag=false;
         while(!q.isEmpty()){
             int size=q.size();
             List<Integer> level=new ArrayList<>();
-            
-            for(int i=0;i<size;i++){
-                TreeNode node=q.remove();
-                if(node.left!=null){
-                    q.add(node.left);
+            Stack<Integer> st=new Stack<>();
+            while(size-->0){
+                TreeNode curr=q.remove();
+                if(flag){
+                    st.push(curr.val);
                 }
-                if(node.right!=null){
-                    q.add(node.right);
+                else{
+                    level.add(curr.val)
                 }
-                level.add(node.val);
+                if(curr.left!=null){
+                    q.add(curr.left);
+                }
+                if(curr.right!=null){
+                    q.add(curr.right);
+                }
             }
-            if(flag){
-                Collections.reverse(level);
-                flag=false;
+            flag=!flag;
+            while(!st.isEmpty()){
+                level.add(st.pop());
             }
-            else{
-                flag=true;
-            }
-            ans.add(level);
+            result.add(level);
         }
-        return ans;
+        return result;
     }
 }
